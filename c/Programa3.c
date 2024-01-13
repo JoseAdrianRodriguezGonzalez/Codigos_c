4f1810cbb7325aeef2a6e5e3d9c8d8385737ca69
Adrian <88957069+JoseAdrianRodriguezGonzalez@users.noreply.github.com>
2024-01-13 16:38:52 -0600

    Update and rename Programa3.cpp to Programa3.c

--- c/Programa3.c
+++ c/Programa3.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<conio.h>
 main(){
-	/*Carácteres especiales*/
+	/*CarÃ¡cteres especiales*/
 	char pre=168,e=144,a=160, o=162;
 	/*Variables*/
 	char name[20],surname1[20],surname2[20]; //nombre
 	char date[9];		//Fecha
-	char street[40],col[40],num[8],city[30],state[30];	//Localización
+	char street[40],col[40],num[8],city[30],state[30];	//LocalizaciÃ³n
 	char telephone[15];		//Telefono
 	
 	/*Ingreso de datos*/
@@ -55,8 +55,8 @@ main(){
 	printf("\n\t\t\tFicha de formulario:Respuestas proporcionadas\n");
 	/*Datos escritos en el formulario*/
 	
-	printf("\n\tNombre: %s %s %s\t\tTelef%cno:%s \n",name,surname1,surname2,o,telephone);//Nombre y telefóno
+	printf("\n\tNombre: %s %s %s\t\tTelef%cno:%s \n",name,surname1,surname2,o,telephone);//Nombre y telefÃ³no
 	printf("\n\tFecha de nacimiento: %s \n" ,date);		//Fecha de nacimiento	
-	printf("\n\tDirecci%cn:%s, %s, #%s, %s, %s, ",o,street,col,num,city,state);//Dirección
+	printf("\n\tDirecci%cn:%s, %s, #%s, %s, %s, ",o,street,col,num,city,state);//DirecciÃ³n
 	getch();		
 }
